91b4a18a85ad0b149aee4b714f201e7aa3fef0b3
cjephuneh <calebjephuneh@gmail.com>
2023-07-09 10:18:30 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+const { ApolloServer } = require("apollo-server");
+
+const server = new ApolloServer();
+
+
+
+server.listen().then(({ url }) => {
+    console.log("the server is ready at " + url);
+
+});
