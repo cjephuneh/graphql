eea4933df1130a011eaaebd18775acfe5a326178
cjephuneh <calebjephuneh@gmail.com>
2023-07-09 19:11:50 +0300

    updates

--- index.js
+++ index.js
@@ -8,11 +8,17 @@ const { ApolloServer, gql } = require("apollo-server");
 //integer
 //float
 //boolean
+//ID
 
 
 const typeDefs = gql`
     type Query { 
         hello: String
+        # hello: String!
+        numberOfAnimals: Int
+        numberOfBooks: Int
+        price: Float
+        isCool: Boolean
     }
 `;
 
@@ -20,7 +26,22 @@ const resolvers = {
     Query: {
         hello: () =>{
             return "Hello World!"
-        }
+        },
+
+        numberOfAnimals: () => {
+            return 55
+        },
+
+        numberOfBooks: () => {
+            return 5
+        },
+
+        price: () => {
+            return 23.23
+        },
+
+        isCool: () =>  true,
+        
     }
 };
 
